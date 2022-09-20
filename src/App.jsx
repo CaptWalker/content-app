42dfe63e453b41ed2ec17f1734d85318f889ce18
CaptWalker <28630298+CaptWalker@user.noreply.github.com>
2022-09-20 13:40:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Content from "./Content/Content";
+
+const App = () => { 
+  
+  return (
+    <div>
+      content
+    </div>
+)};
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    publicPath: "http://localhost:8085/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 8085,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "content_app",
+      filename: "remoteEntry.js",
+      remotes: {
+        AdditionalInformationCapability: 'additional_information_capability@http://localhost:8081/remoteEntry.js',
+        KycCapability: 'kyc_capability@http://localhost:8082/remoteEntry.js',
+        RFI: 'rfi@http://localhost:8083/remoteEntry.js',
+        RFA: 'rfa@http://localhost:8084/remoteEntry.js'
+      },
+      exposes: {
+        "./Content":"./src/Content/Content.jsx"
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
