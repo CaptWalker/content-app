3fe89815085adb00de636c91d90b854857d84444
CaptWalker <28630298+CaptWalker@user.noreply.github.com>
2022-09-22 11:48:20 +0100

    adding modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+const AIC = React.lazy(() => import('AdditionalInformationCapability/AIC'));
+const KycCapability = React.lazy(() => import('KycCapability/KycCapability'));
 import Content from "./Content/Content";
+import { ErrorBoundary } from "./ErrorBoundary/ErrorBoundary";
+
 
 const App = () => { 
+
+    const aic = () => (
+        <ErrorBoundary>
+            <Suspense fallback={null}>
+                <AIC />
+            </Suspense>
+        </ErrorBoundary>
+    )
+
+    const kycCapability = () => (
+        <ErrorBoundary>
+            <Suspense fallback={null}>
+                <KycCapability />
+            </Suspense>
+        </ErrorBoundary>
+    )
+
+  const access = [
+    {
+        id: 'additional_information_capability',
+        name: "Additional Information Capability",
+        appConfig: aic,
+    },
+    {
+        id: 'kyc_capability',
+        name: "KYC Capability",
+        appConfig: kycCapability,
+    },
+]
   
   return (
     <div>
-      content
+      <Content access={access} />
     </div>
 )};
 ReactDOM.render(<App />, document.getElementById("app"));
